cf0bfa904f4fe333ff4efc56cf0fefbaff6e5065
Aleksey Belchenkov <u608110@gmail.com>
2020-11-26 15:41:15 +0300

    Add Router

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import {
+    HashRouter as Router,
+    Switch,
+    Route
+} from 'react-router-dom';
 
 import Home from "./views/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
     return (
-       <Home />
+            <Router>
+                <Navbar />
+                <div className='content-wrapper'>
+                    <Switch>
+                        <Route path="/" exact>
+                            <Home />
+                        </Route>
+                        <Route path="/settings">
+                            Settings
+                        </Route>
+                        <Route path="/register">
+                            Register
+                        </Route>
+                        <Route path="/login">
+                            Login
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
     )
 };
 
--- src/js/components/Navbar.js
+++ src/js/components/Navbar.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="chat-navbar">
             <nav className="chat-navbar-inner">
                 <div className="chat-navbar-inner-left">
-                    <a href="/" className="btn btn-outline-success ml-2">Settings</a>
+                    <Link to="/settings" className="btn btn-outline-success ml-2">Settings</Link>
                 </div>
                 <div className="chat-navbar-inner-right">
                     <span className="logged-in-user">Hi User</span>
-                    <button
+                    <Link
+                        to="/register"
                         onClick={() => {}}
-                        className="btn btn-sm btn-outline-danger ml-2">Logout</button>
-                    <button
+                        className="btn btn-sm btn-outline-primary ml-2"
+                    >Register</Link>
+                    <Link
+                        to="/login"
                         onClick={() => {}}
-                        className="btn btn-sm btn-outline-success ml-2">Login</button>
+                        className="btn btn-sm btn-outline-success ml-2"
+                    >Login</Link>
                 </div>
             </nav>
         </div>
