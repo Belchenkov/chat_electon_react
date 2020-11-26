ca7aceda5f2340b0b38a932bbfaa824fff422085
Aleksey Belchenkov <u608110@gmail.com>
2020-11-26 14:16:47 +0300

    Separation to components

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../scss/index.scss";
-import App from "../App";
+import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById('chatApp'));
