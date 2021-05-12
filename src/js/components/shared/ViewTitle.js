12fdf15b6b434e81f23f76b57ca17769c659fb8d
Aleksey Belchenkov <u608110@gmail.com>
2021-05-12 19:57:29 +0400

    Link to the chat

--- src/js/components/shared/ViewTitle.js
+++ src/js/components/shared/ViewTitle.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ViewTitle = () => {
+const ViewTitle = ({ text }) => {
     return (
         <div className="chat-name-container">
-            <span className="name">Choose your channel</span>
+            <span className="name">{ text }</span>
             <Link
                 to="/"
                 className="btn btn-primary btn-sm back-button"
