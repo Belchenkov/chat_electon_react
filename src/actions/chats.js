9a67eb3511845b8e7f1f817ffb5cd1c95aebc7a6
Aleksey Belchenkov <u608110@gmail.com>
2021-06-20 19:14:35 +0400

    Display chats in the list

--- src/actions/chats.js
+++ src/actions/chats.js
@@ -1,14 +1,6 @@
 import * as api from '../js/api/chats';
 
-export function fetchChats() {
-    return async function (dispatch) {
-        const chats = await api.fetchChats();
-
-        dispatch({
-           type: 'CHATS_FETCH_SUCCESS',
-           chats
-        });
-
-        return chats;
-    }
-}
+export const fetchChats = () => dispatch =>
+    api
+        .fetchChats()
+        .then(chats => dispatch({ type: 'CHATS_FETCH_SUCCESS', chats }));
