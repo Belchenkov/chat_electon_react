6d81ff2163afcfd66e6feb27adc879005735aa01
Aleksey Belchenkov <u608110@gmail.com>
2021-06-26 13:38:02 +0400

    Create User Profile

--- src/js/api/auth.js
+++ src/js/api/auth.js
@@ -1,9 +1,28 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-export const register = async ({email, password}) => {
+import db from '../db/firestore';
+
+const createUserProfile = userProfile => {
+    return db
+        .collection('profiles')
+        .doc(userProfile.uid)
+        .set(userProfile);
+};
+
+export const register = async ({email, password, username, avatar}) => {
     try {
         const { user } = await firebase.auth().createUserWithEmailAndPassword(email, password);
+        const { uid } = user;
+
+        await createUserProfile({
+            uid,
+            username,
+            email,
+            avatar,
+            joinedChats: []
+        });
+
         return user;
     } catch (error) {
         return Promise.reject(error.message);
