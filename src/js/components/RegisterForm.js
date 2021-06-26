88ed07369f8bd984e32c2faf1e226b93c1165429
Aleksey Belchenkov <u608110@gmail.com>
2021-06-26 13:03:38 +0400

    Get value from the form

--- src/js/components/RegisterForm.js
+++ src/js/components/RegisterForm.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 export default function RegisterForm() {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        alert(JSON.stringify(data));
+    };
+
     return (
-        <form onSubmit={() => {}} className="centered-container-form">
+        <form onSubmit={handleSubmit(onSubmit)} className="centered-container-form">
                     <div className="header">Create an account</div>
                     <div className="form-container">
                         <div className="form-group">
                             <label htmlFor="email">Email</label>
                             <input
+                                {...register('email', { required: true })}
                                 type="email"
                                 className="form-control"
                                 name="email"
@@ -18,6 +26,7 @@ export default function RegisterForm() {
                         <div className="form-group">
                             <label htmlFor="username">Username</label>
                             <input
+                                {...register('username', { required: true })}
                                 type="text"
                                 name="username"
                                 className="form-control"
@@ -27,6 +36,7 @@ export default function RegisterForm() {
                         <div className="form-group">
                             <label htmlFor="avatar">Avatar</label>
                             <input
+                                {...register('avatar', { required: false })}
                                 type="text"
                                 name="avatar"
                                 className="form-control"
@@ -36,6 +46,7 @@ export default function RegisterForm() {
                         <div className="form-group">
                             <label htmlFor="password">Password</label>
                             <input
+                                {...register('password', { required: true })}
                                 name="password"
                                 type="password"
                                 className="form-control"
