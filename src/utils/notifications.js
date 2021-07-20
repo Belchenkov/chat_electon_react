1c3df1302a51d3183a454080cc0e8790981629be
Aleksey Belchenkov <u608110@gmail.com>
2021-07-20 14:36:32 +0400

    Display Notice

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -2,6 +2,8 @@ export default {
     setup() {
         if (! ('Notification' in window)) {
             console.error("This browser window doesn't support notification!");
+        } else if (Notification.permission !== 'granted') {
+            return;
         } else if (Notification.permission !== 'denied') {
             Notification.requestPermission()
                 .then(permission => {
@@ -10,5 +12,8 @@ export default {
                     }
                 })
         }
+    },
+    show({ title, body }) {
+        new Notification(title, { body });
     }
 }
