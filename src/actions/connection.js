902ff837bfac40cde01baa52a12f7cc649c2fe12
Алексей Бельченков <u608110@gmail.com>
2024-03-23 09:08:11 +0400

    save user online status to firebase

--- src/actions/connection.js
+++ src/actions/connection.js
@@ -1,7 +1,8 @@
 import * as api from '../js/api/connection';
 
-export const checkUserConnection = () => dispatch => {
+export const checkUserConnection = uid => dispatch => {
     api.onConnectionChanged(isConnected => {
+        api.setUserOnlineStatus(uid, isConnected).then();
         dispatch({ type: 'CONNECTION_USER_STATUS_CHANGED'});
     });
 };
